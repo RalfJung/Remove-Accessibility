23d814b8e4a6565bfe634e47f3014d46176de19a
Ralf Jung <post@ralfj.de>
2024-08-02 10:35:17 +0200

    Gnome 46 compatibility

--- extension.js
+++ extension.js
@@ -18,13 +18,13 @@
  along with Remove Accessibility.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
-const Lang = imports.lang;
-const Main = imports.ui.main;
+//const Lang = imports.lang;
+//const Main = imports.ui.main;
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
-const A11yStatusIconHelper = new Lang.Class({
-    Name: 'A11yStatusIconHelper',
-
-    _init: function() {
+class A11yStatusIconHelper {
+    constructor() {
         // Get a reference to the right accessibility icon.
         let a11yElement = null;
         if (typeof Main.panel._statusArea === 'undefined') {
@@ -34,45 +34,44 @@ const A11yStatusIconHelper = new Lang.Class({
         }
 
         // The ClutterActor representing the icon in the panel.
-        this._iconActor = a11yElement.actor;
+        this._iconActor = a11yElement /* .actor */ ; // "usage of actor is deprecated"
 
         // Id for the handler used to connect to GSetting's signals.
         this._handlerId = 0;
-    },
+    }
 
-    _onIconVisibilityChanged: function(actor) {
+    _onIconVisibilityChanged(actor) {
         if (actor.is_visible()) {
             actor.hide();
         }
-    },
+    }
 
-    hideA11yElement: function() {
+    hideA11yElement() {
         // Monitor changes to the icon's visibilty, so that we
         // ensure the icon always keeps hidden, no-matter-what.
         this._handlerId = this._iconActor.connect('notify::visible',
-                                                  Lang.bind(this, this._onIconVisibilityChanged));
+                                                  (a) => this._onIconVisibilityChanged(a));
         this._iconActor.hide();
-    },
+    }
 
-    restoreA11yElement: function() {
+    restoreA11yElement() {
         if (this._handlerId) {
             this._iconActor.disconnect(this._handlerId);
             this._handlerId = 0;
         }
         this._iconActor.show();
     }
-});
+};
 
 let _a11yHelper = null;
 
-function init() {
-}
-
-function enable() {
-    _a11yHelper = new A11yStatusIconHelper();
+export default class RemoveAccessibility extends Extension {
+    enable() {
+        _a11yHelper = new A11yStatusIconHelper();
     _a11yHelper.hideA11yElement();
-}
+    }
 
-function disable() {
-    _a11yHelper.restoreA11yElement();
+    disable() {
+        _a11yHelper.restoreA11yElement();
+    }
 }
